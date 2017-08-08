32f79e9be2852525c90ea02082ba55ce978b7995
dvoinyakov <mishadvoinyakov@gmail.com>
2017-08-08 20:28:38 +0300

    0.1

---
+++ app/js/common.js
@@ -0,0 +1,11 @@
+$(function() {
+
+    $(".slider-items").owlCarousel({
+        items: 1,
+        loop: true,
+        nav: true,
+        smartSpeed: 700,
+        navText: ['дальше','prev']
+    });
+
+});
